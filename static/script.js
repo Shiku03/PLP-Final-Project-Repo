8d9559900326ae7382c637cac3c74d1f7801199f
Shiku03 <nikki.shiku@gmail.com>
2025-11-19 13:22:10 +0300

    renamed and moved to static folder

---
+++ static/script.js
@@ -0,0 +1,13 @@
+//redirect to sign-in page function
+function redirectToPage(url){
+    window.location.href=url;
+}
+//show sign-in form function
+
+//sign-in form validation function
+
+
+
+//generate video function
+
+//upload files function
